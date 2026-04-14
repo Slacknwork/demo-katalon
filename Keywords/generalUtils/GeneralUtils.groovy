0978208661f557e68e256394bfaf698c7a630893
Hi <Hi@DESKTOP-QDSGC3E>
2026-04-14 16:32:43 +0700

    change file and function name

--- Keywords/generalUtils/GeneralUtils.groovy
+++ Keywords/generalUtils/GeneralUtils.groovy
@@ -9,45 +9,29 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 import internal.GlobalVariable
 
-//General steps to use in page
 public class GeneralUtils {
 	
 	//Open login form in home page
 	@Keyword
-	public static void openLoginForm() {
+	static def openLoginForm() {
 		//Click avatar icon
 		loginUtils.LoginElements.clickElement(findTestObject('LoginFormElements/icon_Avatar'))
 		
 		//Click login button in popup menu
-		loginUtils.LoginElements.clickElement(findTestObject('ImgAvatarMenu/button_Login'))
+		loginUtils.LoginElements.clickElement(findTestObject('ImgAvatarPopupMenu/button_Login'))
 	}
 	
 	//Open register form through in home page
 	@Keyword
-	public static void openRegisterFormThroughLoginForm() {
+	static def openRegisterFormInLoginForm() {
 		//Click avatar icon
 		loginUtils.LoginElements.clickElement(findTestObject('LoginFormElements/icon_Avatar'))
 		
 		//Click login button in popup menu
-		loginUtils.LoginElements.clickElement(findTestObject('ImgAvatarMenu/button_Login'))
+		loginUtils.LoginElements.clickElement(findTestObject('ImgAvatarPopupMenu/button_Login'))
 		
 		//Click register button in login form
 		loginUtils.LoginElements.clickElement(findTestObject('LoginFormElements/button_Register'))
 	}
-	
-//	@Keyword
-//	static def implementTestData(String field, String status){
-//		String input = ""
-//		def data = TestDataFactory.findTestData('Data Files/LoginData')
-//		int rowCount = data.getRowNumbers()
-//		
-//		for (int i = 1; i <= rowCount; i++) {
-//			if (data.getValue('Status', i).equals(status)) {
-//				input = data.getValue(field, i)
-//				break
-//			}
-//		}	
-//		return input	
-//	}
 	
 }
---
+++ Object Repository/LoginFormElements/ErrorMessage/text_EmptyPassword.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>text_EmptyPassword</name>
+   <tag></tag>
+   <elementGuidId>9ceb07b6-1e50-4228-b99e-80b3a02eac58</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='password']/following-sibling::p[text()='Vui lòng không bỏ trống']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
